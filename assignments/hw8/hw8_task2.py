5e9a8887ace206d81fdf91795554e8e62b111ede
Olha Dubkova <dubkova.om@gmail.com>
2026-03-13 10:12:16 +0200

    Homework 8 completed

---
+++ assignments/hw8/hw8_task2.py
@@ -0,0 +1,14 @@
+# Task 2
+
+def make_country(name, capital):
+    country = {
+        "name": name,
+        "capital": capital
+    }
+
+    print(country["name"])
+    print(country["capital"])
+
+
+# function call example
+make_country("Ukraine", "Kyiv")
