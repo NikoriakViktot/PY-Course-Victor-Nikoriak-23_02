68485a1491033517b6dcd1629bd7528dbe090b7b
Sergiy Chernyshov <shadowtgrad7@gmail.com>
2026-03-24 20:31:56 +0200

    Homework 03 to correct folder

---
+++ homework-03/homework-03.py
@@ -0,0 +1,43 @@
+#task1
+#"Good day <name>! <day> is a perfect day to learn some python."
+name = "Sergiy"
+day = "25.02.2026"
+print (f'Good day "{name}"! "{day}" is a perfect day to learn some python.' )
+
+print ("\n")
+
+#task2
+# manipulate strings.
+
+# Save your first and last name as separate variables, then use string concatenation to add them together with a white space in between and a greeting.
+
+first_name = "Sergiy"
+last_name = "Chernyshov"
+print (f'{first_name} {last_name}')
+
+print (f'Hello {first_name} {last_name}!')
+
+print("\n")
+
+#Task3
+#Using python as a calculator.
+
+# Make a program with 2 numbers saved in separate variables a and b, then print the result for each of the following:
+# Addition Subtraction Division Multiplication Exponent (Power) Modulus Floor division
+
+a=6
+b=3
+
+print(a+b)
+
+print(a-b)
+
+print(a*b)
+
+print(a/b)
+
+print(a//b)
+
+print(a%b)
+
+print(a**b)
