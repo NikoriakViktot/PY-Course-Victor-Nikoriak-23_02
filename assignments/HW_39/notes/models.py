c8e568d7fb8d089016e90b767f91581c7efb7c38
Husak Alla <allo4ka.gusak@gmail.com>
2026-06-04 23:40:16 +0300

    HW_40

--- assignments/HW_39/notes/models.py
+++ assignments/HW_39/notes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Назва категорії")
+
+    def __str__(self):
+        return self.title
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    text = models.TextField(verbose_name="Текст нотатки")
+    reminder = models.DateTimeField(null=True, blank=True, verbose_name="Час нагадування")
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Категорія")
+
+    def __str__(self):
+        return self.title
--- assignments/HW_39/notes/views.py
+++ assignments/HW_39/notes/views.py
@@ -1,26 +1,8 @@
 from django.shortcuts import render
+from .models import Note
 
 
 def notes_index(request):
-    notes_list = [
-        {
-            "id": 1,
-            "title": "Теги розмітки HTML",
-            "content": "HTML — це скелет сайту. Головне запам'ятати блочні теги (div, h1, p) та рядкові (span, a).",
-            "date": "04.06.2026"
-        },
-        {
-            "id": 2,
-            "title": "Стилізація через CSS",
-            "content": "CSS — це краса сайту. Вчимося міняти кольори, додавати тіні (box-shadow) та будувати гнучку сітку Grid.",
-            "date": "04.06.2026"
-        },
-        {
-            "id": 3,
-            "title": "Статика в Django",
-            "content": "Для підключення CSS-файлів обов'язково використовуємо тег {% load static %} на самому початку HTML-документа.",
-            "date": "04.06.2026"
-        }
-    ]
+    notes_from_db = Note.objects.all().select_related('category')
 
-    return render(request, 'notes/index.html', {'notes': notes_list})
+    return render(request, 'notes/index.html', {'notes': notes_from_db})
