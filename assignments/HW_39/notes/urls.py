d0f6ca461638621134371c1e92531d7f9274d667
Husak Alla <allo4ka.gusak@gmail.com>
2026-06-05 21:17:43 +0300

    HW_41

--- assignments/HW_39/notes/urls.py
+++ assignments/HW_39/notes/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.notes_index, name='notes_index'),
+    path('create/', views.note_create, name='note_create'),
+    path('<int:note_id>/', views.note_detail, name='note_detail'),
+    path('<int:note_id>/delete/', views.note_delete, name='note_delete'),
 ]
